ac03e9efcd1553df58b3707320169a70c0a7b267
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-19 12:22:56 +0000

    helpers functions

---
+++ misc.py
@@ -0,0 +1,15 @@
+import numpy as np
+import PIL
+
+
+def prediction_to_image(arr):
+    "converts numpy array from model prediction to PIL image.
+
+    Parameters
+        ----------
+            arr : ndarray 
+                rgb image in array form to be converted "
+    arr = np.squeeze((img_pr[0]+img_pr[1]+img_pr[2])/3)
+    arr = arr/arr.max()*255
+    im = PIL.Image.fromarray(np.uint8(arr))
+    return im
