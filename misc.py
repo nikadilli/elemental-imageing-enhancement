181b4b72364cc1cea71f13b6a0eefcf22709a1f4
nikadilli <nikadilli@gmail.com>
2019-12-02 10:17:22 +0000

    convert la to img

--- misc.py
+++ misc.py
@@ -14,3 +14,21 @@ def prediction_to_image(arr):
     arr = arr/arr.max()*255
     im = PIL.Image.fromarray(np.uint8(arr))
     return im
+
+def ablation_to_image(path, p=99):
+    """converts csv from laser ablation to image clipping out outliers.
+
+    Parameters
+    ----------
+    path : str or path 
+        path to csv file
+    p: float
+        percentile to remove outliers from laser """
+    
+    savename = re.sub('\..*$', '', filename)+'.png'
+    im = np.genfromtxt(path, delimiter=',')
+    perc = np.percentile(im, p)
+    im = np.where(im<perc, im, perc) 
+    im = im/im.max()*255
+    im = im.astype(np.uint8)
+    imageio.imwrite(path + savename, im)
